838073d71e64f593540e5abb0cf00bccee6e9419
Sergeyengibaryan <sergey.y@tumo.org>
2023-10-14 13:38:30 +0400

    sercond commit

--- script.js
+++ script.js
@@ -7,26 +7,7 @@ var predArr = []
 var kaytakArr = [] 
 var chariqArr = [] 
 var bariqArr = []
-// function setup() {
-//     frameRate(1);
-//     createCanvas(matrix[0].length * side, matrix.length * side);
-//     background('#acacac');
-//     for (var y = 0; y < matrix.length; ++y) {
-//         for (var x = 0; x < matrix[y].length; ++x) {
-//             if (matrix[y][x] == 1) {
-//                 var grass = new Grass(x, y, 1);
-//                 grassArr.push(gr);
-//             }
-//             if (matrix[y][x] == 2) {
-//                 var grassEater = new grassEater(x, y, 1);
-//                 grassEaterArr.push(gr);
-//             }
 
-//         }
-
-//     }
-
-// }
 function setup () {  
     function matrixGenerator(size, countGrass, countGrassEater, predatorCount, kaytakCount, chariqCount,bariqCount) {
         for (let i = 0; i < size; i++) {
@@ -151,29 +132,6 @@ function setup () {
    
 }
 
-// function draw() {
-
-//     for (var y = 0; y < matrix.length; y++) {
-
-//         for (var x = 0; x < matrix[y].length; x++) {
-
-//             if (matrix[y][x] == 1) {
-//                 fill("green");
-//                 rect(x * side, y * side, side, side);
-//             }
-
-//             else if (matrix[y][x] == 0) {
-//                 fill("#acacac");
-//                 rect(x * side, y * side, side, side);
-//             }
-//             else if (matrix[y][x] == 2) {
-//                 fill("yellow");
-//                 rect(x * side, y * side, side, side);
-//             }
-//         }
-
-
-//     }
 
 function draw() {
     for (var y = 0; y < matrix.length; y++) {
@@ -239,20 +197,7 @@ function draw() {
             chariqArr[i].mul() 
             
         } 
-    // for (var i = 0; i < chariqArr.length; i++) { 
-    //     chariqArr[i].move() 
-       
-        
-    // } 
-    // for (var i = 0; i < bariqArr.length; i++) { 
-    //     bariqArr[i].eat() 
-       
-        
-    // }  
-    // for (var i = 0; i < bariqArr.length; i++) { 
-    //     bariqArr[i].mul() 
-       
-    // } 
+
     setTimeout(function(){
         for (var i = 0; i < chariqArr.length; i++) {
             chariqArr[i].eat()   
